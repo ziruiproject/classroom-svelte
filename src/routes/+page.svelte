ec00e0f34acf574d52abdfba7fa26eaf8a3da7a0
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-18 13:22:14 +0700

    added firebase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+	import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+	const auth = getAuth();
+	createUserWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed up
+			const user = userCredential.user;
+			// ...
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
