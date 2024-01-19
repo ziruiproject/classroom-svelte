edfc6f6199e7c74228e0b97ced45ffec570d0f83
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-19 08:20:23 +0700

    added course creator

--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,4 +1,8 @@
 <script>
+	import { db } from '../../firebase';
+	import { getDoc, collection } from 'firebase/firestore';
+
+	async function show() {}
 </script>
 
 <h1>Courses</h1>
--- src/routes/courses/create/+page.svelte
+++ src/routes/courses/create/+page.svelte
@@ -1,16 +1,31 @@
 <script>
-	function place() {
-		return 1;
+	import { db } from './../../../firebase.js';
+	import { addDoc, collection } from 'firebase/firestore';
+
+	let title, date, time;
+
+	async function create() {
+		try {
+			const docRef = await addDoc(collection(db, 'courses'), {
+				title: title,
+				date: date,
+				time: time
+			});
+		} catch (e) {
+			console.error('gagal gan: ', e);
+		}
 	}
 </script>
 
-<form on:submit|preventDefault={place}>
+<form on:submit|preventDefault={create}>
 	<label for="title">Judul</label>
-	<input type="text" name="title" />
+	<input type="text" name="title" bind:value={title} />
 	<br />
 	<label for="date">Tanggal</label>
-	<input type="date" name="date" />
+	<input type="date" name="date" bind:value={date} />
 	<br />
 	<label for="time">Waktu</label>
-	<input type="time" name="time" />
+	<input type="time" name="time" bind:value={time} />
+	<br />
+	<button type="submit">Buat</button>
 </form>
