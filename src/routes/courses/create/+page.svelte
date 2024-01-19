eba77d1bee15a7f831692b3594a457b8709a146f
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-19 07:03:19 +0700

    added course creator

---
+++ src/routes/courses/create/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	function place() {
+		return 1;
+	}
+</script>
+
+<form on:submit|preventDefault={place}>
+	<label for="title">Judul</label>
+	<input type="text" name="title" />
+	<br />
+	<label for="date">Tanggal</label>
+	<input type="date" name="date" />
+	<br />
+	<label for="time">Waktu</label>
+	<input type="time" name="time" />
+</form>
