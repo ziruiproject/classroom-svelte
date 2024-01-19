4397604686daa3d300516770483f1915a31b0ce2
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-19 09:54:49 +0700

    added course index

--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,8 +1,40 @@
 <script>
+	import { onMount } from 'svelte';
 	import { db } from '../../firebase';
-	import { getDoc, collection } from 'firebase/firestore';
+	import { getDocs, collection } from 'firebase/firestore';
 
-	async function show() {}
+	let courses = [];
+	let course;
+
+	onMount(async () => {
+		const querySnapshot = await getDocs(collection(db, '/courses'));
+		querySnapshot.forEach((doc) => {
+			console.log(doc.data());
+			courses.push(doc.data());
+			courses = courses;
+			console.log(courses);
+		});
+	});
 </script>
 
 <h1>Courses</h1>
+{#if courses}
+	<h1>real</h1>
+	{#each courses as course}
+		<h1>{course.title}</h1>
+		<h1>{typeof course}</h1>
+	{/each}
+	<h1>{typeof courses}</h1>
+{:else}
+	<h1>gada masbro</h1>
+{/if}
+<button
+	on:click={() => {
+		console.log(courses);
+		courses.forEach((course) => {
+			console.log(course);
+		});
+	}}
+>
+	Click
+</button>
