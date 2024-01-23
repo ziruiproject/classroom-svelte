46466dfa2db5588bb81d4943374300cea4c00067
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-23 08:24:15 +0700

    buillding google button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -21,35 +21,42 @@
 	}
 </script>
 
-<div class="text-white flex items-center">
-	<div class="p-1 border-[2px] border-light-gray rounded-2xl">
-		<svg
-			width="32px"
-			height="32px"
-			stroke-width="1.5"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				d="M15 6L9 12L15 18"
-				class="stroke-white"
+<main class="md:w-1/4 mx-auto my-auto">
+	<div class="flex text-white items-center">
+		<button class="md:hidden block p-1 border-[3px] border-light-gray rounded-2xl">
+			<svg
+				width="32px"
+				height="32px"
 				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			></path>
-		</svg>
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					d="M15 6L9 12L15 18"
+					class="stroke-white"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				></path>
+			</svg>
+		</button>
+		<h1 class="text-4xl font-bold pl-6 md:pl-0">Log in</h1>
 	</div>
-	<h1 class="text-4xl font-bold pl-6">Log in</h1>
-</div>
 
-<form on:submit|preventDefault={login} class="pt-12 grid gap-y-8 w-full">
-	<div class="w-full grid gap-y-4">
-		<label for="email" class="label">Email</label>
-		<input type="text" name="email" bind:value={email} class="form-input" />
+	<div>
+		<span>Login with one of the following options.</span>
+		<button class="form hover:border-green">Google</button>
 	</div>
-	<div class="w-full grid gap-y-4">
-		<label for="password" class="label">Password</label>
-		<input type="password" name="password" bind:value={password} class="form-input" />
-	</div>
-	<button type="submit" class="button">Log in</button>
-</form>
+
+	<form on:submit|preventDefault={login} class="pt-12 grid gap-y-8 w-full">
+		<div class="w-full grid gap-y-4">
+			<label for="email" class="label">Email</label>
+			<input type="text" name="email" bind:value={email} class="form" />
+		</div>
+		<div class="w-full grid gap-y-4">
+			<label for="password" class="label">Password</label>
+			<input type="password" name="password" bind:value={password} class="form" />
+		</div>
+		<button type="submit" class="button">Log in</button>
+	</form>
+</main>
