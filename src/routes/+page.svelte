3b8910ce71bf38abb4fd9948eeabf16b70685db3
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-27 11:18:31 +0700

    added logout button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { auth } from '$lib';
+	import { signOut } from 'firebase/auth';
+
 	export let data;
 	let user = data.user,
 		enrolled = data.enrolled,
@@ -9,6 +13,12 @@
 	photoProfile =
 		user?.photoProfile ||
 		`https://placehold.co/500x500/green/white/?text=${user?.displayName?.[0]}`;
+
+	async function logout() {
+		signOut(auth).then(() => {
+			goto('/login');
+		});
+	}
 </script>
 
 <svelte:head>
@@ -24,33 +34,59 @@
 				<span class="text-text-gray text-lg font-light">Semangat Belajar</span>
 			</div>
 		</div>
-		<div class="bg-green rounded-xl p-3">
-			<svg
-				width="24px"
-				height="24px"
-				stroke-width="1.5"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				color="#ffffff"
-				><path
-					d="M14 20.4V14.6C14 14.2686 14.2686 14 14.6 14H20.4C20.7314 14 21 14.2686 21 14.6V20.4C21 20.7314 20.7314 21 20.4 21H14.6C14.2686 21 14 20.7314 14 20.4Z"
-					stroke="#ffffff"
+		<div class="gap-x-3 flex">
+			<button on:click={logout} class="bg-[#7c1f2a] rounded-xl p-3">
+				<svg
+					width="24px"
+					height="24px"
 					stroke-width="1.5"
-				></path><path
-					d="M3 20.4V14.6C3 14.2686 3.26863 14 3.6 14H9.4C9.73137 14 10 14.2686 10 14.6V20.4C10 20.7314 9.73137 21 9.4 21H3.6C3.26863 21 3 20.7314 3 20.4Z"
-					stroke="#ffffff"
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					color="#ffffff"
+					><path
+						d="M12 12H19M19 12L16 15M19 12L16 9"
+						stroke="#ffffff"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path><path
+						d="M19 6V5C19 3.89543 18.1046 3 17 3H7C5.89543 3 5 3.89543 5 5V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V18"
+						stroke="#ffffff"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path></svg
+				>
+			</button>
+			<div class="bg-green rounded-xl p-3">
+				<svg
+					width="24px"
+					height="24px"
 					stroke-width="1.5"
-				></path><path
-					d="M14 9.4V3.6C14 3.26863 14.2686 3 14.6 3H20.4C20.7314 3 21 3.26863 21 3.6V9.4C21 9.73137 20.7314 10 20.4 10H14.6C14.2686 10 14 9.73137 14 9.4Z"
-					stroke="#ffffff"
-					stroke-width="1.5"
-				></path><path
-					d="M3 9.4V3.6C3 3.26863 3.26863 3 3.6 3H9.4C9.73137 3 10 3.26863 10 3.6V9.4C10 9.73137 9.73137 10 9.4 10H3.6C3.26863 10 3 9.73137 3 9.4Z"
-					stroke="#ffffff"
-					stroke-width="1.5"
-				></path></svg
-			>
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					color="#ffffff"
+					><path
+						d="M14 20.4V14.6C14 14.2686 14.2686 14 14.6 14H20.4C20.7314 14 21 14.2686 21 14.6V20.4C21 20.7314 20.7314 21 20.4 21H14.6C14.2686 21 14 20.7314 14 20.4Z"
+						stroke="#ffffff"
+						stroke-width="1.5"
+					></path><path
+						d="M3 20.4V14.6C3 14.2686 3.26863 14 3.6 14H9.4C9.73137 14 10 14.2686 10 14.6V20.4C10 20.7314 9.73137 21 9.4 21H3.6C3.26863 21 3 20.7314 3 20.4Z"
+						stroke="#ffffff"
+						stroke-width="1.5"
+					></path><path
+						d="M14 9.4V3.6C14 3.26863 14.2686 3 14.6 3H20.4C20.7314 3 21 3.26863 21 3.6V9.4C21 9.73137 20.7314 10 20.4 10H14.6C14.2686 10 14 9.73137 14 9.4Z"
+						stroke="#ffffff"
+						stroke-width="1.5"
+					></path><path
+						d="M3 9.4V3.6C3 3.26863 3.26863 3 3.6 3H9.4C9.73137 3 10 3.26863 10 3.6V9.4C10 9.73137 9.73137 10 9.4 10H3.6C3.26863 10 3 9.73137 3 9.4Z"
+						stroke="#ffffff"
+						stroke-width="1.5"
+					></path></svg
+				>
+			</div>
 		</div>
 	</div>
 	<div class="gap-x-4 caraousel grid w-full grid-flow-col">
